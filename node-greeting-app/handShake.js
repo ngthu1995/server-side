1093d8cae33101c4021043411f196659073d2cf3
Thu Nguyen <ngthu1995@gmail.com>
2019-03-02 00:15:04 -0500

    complete basic Node apps

---
+++ node-greeting-app/handShake.js
@@ -0,0 +1,7 @@
+exports.deadFish = function() {
+  console.log("Out of energy");
+};
+
+exports.knuckleCruncher = function() {
+  console.log("Over control");
+};
---
+++ node-greeting-app/main.js
@@ -0,0 +1,15 @@
+const greetings = require("./greeting.js");
+
+const handShake = require("./handShake.js");
+
+handShake.deadFish();
+
+handShake.knuckleCruncher();
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
